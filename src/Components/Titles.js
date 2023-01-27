b5c614118bc9ab0bb47e116affc06d74636e5676
Abe <abeApple@protonmail.com>
2023-01-27 09:34:19 -0500

    added search feature in titles page

--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -1,17 +1,80 @@
-import React from 'react'
-import cubeField from '../Images/cubeField.gif'
+import React, { useRef, useState } from 'react'
+import cubeField from '../Images/cubeField.gif'   
 
 function Titles(props) {  
+  const [filteredNoteArray, setFilteredNoteArray] = useState()
+  const [showSearchResults, setShowSearchResults] = useState(false)
+  const [titleMatches, setTitleMatches] = useState()
+  const [contentMatches, setContentMatches] = useState()
+  const searchInput = useRef()
+
+  function filterNotesForSearch(){
+    var searchValue = searchInput.current.value
+    searchValue = searchValue.toLowerCase()
+
+    // If there is a non empty search value search for matches
+    if(!searchValue || searchValue.replace(" ","") === ""){
+      setShowSearchResults(false)
+      return
+    }    
+
+    var titleMatches = []
+    var contentMatches = []
+    props.noteArray.forEach(note => {
+      if(note.title?.toLowerCase()?.includes(searchValue))
+        titleMatches.push(note)
+      if(note.content?.toLowerCase()?.includes(searchValue))
+        contentMatches.push(note)
+    })
+    setTitleMatches(titleMatches)
+    setContentMatches(contentMatches)
+    setShowSearchResults(true)
+  }
+
   return (
     <div className='titlesContainer'>
-        <div className='titleBox' onClick={(event) => props.editNote(null, event)}>
-          <div className='titleBoxInner newNoteBox'>
-            <div>
-              New Note
+        <div className='searchInput'>
+          <input ref={searchInput} onChange={filterNotesForSearch} placeholder="Search"></input>
+        </div>
+        {showSearchResults?
+          <div>
+            <div className='searchResults'>
+              <div className='searchResultsTitle'>Title Matches</div>
+              {titleMatches.map(noteData => (
+              <div className='titleBox' onClick={() => props.openNote(noteData)}>
+                <div className='titleBoxInner'>
+                  {noteData.title}              
+                </div>
+                  <div className='editButton' onClick={(event) => props.editNote(noteData, event)}>
+                    Edit
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className='searchResults'>
+              <div className='searchResultsTitle'>Content Matches</div>
+              {contentMatches.map(noteData => (
+              <div className='titleBox' onClick={() => props.openNote(noteData)}>
+                <div className='titleBoxInner'>
+                  {noteData.title}              
+                </div>
+                  <div className='editButton' onClick={(event) => props.editNote(noteData, event)}>
+                    Edit
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-        </div>
-        {props.noteArray.map(noteData => (
+        :
+        <div>
+          <div className='titleBox' onClick={(event) => props.editNote(null, event)}>
+            <div className='titleBoxInner newNoteBox'>
+              <div>
+                New Note
+              </div>
+            </div>
+          </div>
+          {props.noteArray.map(noteData => (
           <div className='titleBox' onClick={() => props.openNote(noteData)}>
             <div className='titleBoxInner'>
               {noteData.title}              
@@ -20,7 +83,9 @@ function Titles(props) {
                 Edit
               </div>
             </div>
-        ) )}
+          ))}
+        </div>
+        }
     </div>
   )
 }
